78c812ef1ba9343165e86679ce568bece15b0816
LudovicoMohr <132920559+LudovicoMohr@users.noreply.github.com>
2024-09-30 09:50:27 -0300

    up

---
+++ src/components/NavBar/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import logo from '../../../assets/images/logo_fagro.png'
+import '../../../assets/css/main.css'
+
+const Logo = () => {
+  return <div><img className="logo" src={logo} /></div>;
+};
+export default Logo;
