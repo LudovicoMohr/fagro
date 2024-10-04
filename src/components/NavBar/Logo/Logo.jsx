cf361d4f6c8d721c64643a2621afd5363e186df6
LudovicoMohr <132920559+LudovicoMohr@users.noreply.github.com>
2024-10-04 12:06:33 -0300

    ip

--- src/components/NavBar/Logo/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import logo from '../../../assets/images/logo_fagro.png'
-import '../../../assets/css/main.css'
-
-const Logo = () => {
-  return <div><img className="logo" src={logo} /></div>;
-};
-export default Logo;
